04259f1587c1035382937dccc2129408331db6fc
cyyeh <chihyu.jimmy.yeh@gmail.com>
2020-06-09 14:07:49 +0800

    fix scrolling smooth issue

--- docs/script.js
+++ docs/script.js
@@ -63,14 +63,9 @@ const predictDogBreeds = async imgBase64 => {
   const predictionResults = await apiResponse.json()
   progressBar.classList.toggle('hidden', true)
   dealingWithPredictions(predictionResults)
-  setTimeout(
-    window.scrollTo({
-      left: 0,
-      top: document.body.scrollHeight,
-      behavior: 'smooth'
-    }),
-    100
-  )
+  predictionResultsContainer.scrollIntoView({
+    behavior: 'smooth'
+  })
 }
 
 // callback for dog classification api
